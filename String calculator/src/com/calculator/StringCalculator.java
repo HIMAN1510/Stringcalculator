2b780485a07ea614ca434c5146cc36405eb1d64f
HIMAN1510 <1510himanshu@gmail.com>
2021-09-24 19:30:23 +0530

    Adding the code covering all possible scenario

--- String calculator/src/com/calculator/StringCalculator.java
+++ String calculator/src/com/calculator/StringCalculator.java
@@ -1,16 +1,73 @@
 package com.calculator;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 public class StringCalculator {
 
 	public static int add(String stringNumber) {
 
 		if (!stringNumber.isEmpty()) {
+			String[] stringNums = splitString(stringNumber);
 			int sum = 0;
+			List<Integer> numbers = stringToInt(stringNums);
+			checkForNegative(numbers);
+			for (Integer integer : numbers) {
+				sum += integer;
+			}
 			return sum;
 		}
 		return 0;
 	}
 
-	
+	/**
+	 * function to split the string.
+	 * 
+	 * @param stringNumbers
+	 * @return
+	 */
+	public static String[] splitString(String stringNumbers) {
+		if (stringNumbers.startsWith("//")) {
+			Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(stringNumbers);
+			if (matcher.matches()) {
+				String delimiter = matcher.group(1);
+				String toSplit = matcher.group(2);
+				return toSplit.split(delimiter);
+			}
+			throw new RuntimeException("wrong custom delimeter format");
+		}
+		return stringNumbers.split(",|\n");
+	}
+
+	/**
+	 * function to convert string to int
+	 * 
+	 * @param stringNumbers
+	 * @return
+	 */
+	public static List<Integer> stringToInt(String[] stringNumbers) {
+		List<Integer> numbers = Arrays.stream(stringNumbers).map(Integer::parseInt).collect(Collectors.toList());
+		return numbers;
+	}
+
+	/**
+	 * function to check for negative; if found then throwing exception
+	 * 
+	 * @param numbers
+	 */
+	public static void checkForNegative(List<Integer> numbers) {
+
+		StringBuilder builder = new StringBuilder();
+		for (Integer integer : numbers) {
+			if (integer < 0)
+				builder.append(integer).append(" ");
+		}
+		if (!builder.isEmpty()) {
+			throw new RuntimeException("negative is not allowed " + builder.toString());
+		}
+	}
 
 }
--- String calculator/test/com/calculator/StringCalculatorTest.java
+++ String calculator/test/com/calculator/StringCalculatorTest.java
@@ -11,5 +11,48 @@ class StringCalculatorTest {
 		assertEquals(StringCalculator.add(""), 0);
 	}
 
-	
+	@Test
+	void testAddWithOneNumber() {
+		assertEquals(StringCalculator.add("4"), 4);
+	}
+
+	@Test
+	void testAddWithTwonumber() {
+		assertEquals(StringCalculator.add("4,5"), 9);
+	}
+
+	@Test
+	void testAddWithUnknownnumber() {
+		assertEquals(StringCalculator.add("4,5,1"), 10);
+	}
+
+	@Test
+	void testAddWithNumberSplitByComaAndNewlines() {
+		assertEquals(StringCalculator.add("4,5\n1"), 10);
+	}
+
+	@Test
+	void testAddWithException() {
+		assertThrows(NumberFormatException.class, () -> {
+			StringCalculator.add("4,5,\n1");
+		});
+	}
+
+	@Test
+	void testAddWithNegativeNumber() {
+		assertThrows(RuntimeException.class, () -> {
+			StringCalculator.add("4,-5,-1");
+		});
+	}
+
+	@Test
+	void testAddWithWrongDelimiter() {
+		assertThrows(RuntimeException.class, () -> {
+			StringCalculator.add("//4,-5,-1");
+		});
+	}
+	@Test
+	void testAddWithCustomDelimiter() {
+		assertEquals(StringCalculator.add("//;\n5;2;3"), 10);
+	}
 }
